3437e91100e214c9d17b19fcb9f623f114764660
Maryna Matsveyenka <marina.matweenko1994@gmail.com>
2024-12-09 21:51:23 +0100

    add updating on angular version

--- src/app/components/contact-list/contact-list.component.ts
+++ src/app/components/contact-list/contact-list.component.ts
@@ -1,34 +1,38 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {Contact} from '../../model/contact';
 import {ContactListElementComponent} from './contact-list-element/contact-list-element.component';
+import {NgbAlert} from '@ng-bootstrap/ng-bootstrap';
 
-const CONTACTS : Contact [] = [
+const CONTACTS: Contact[] = [
   {
     name: "Vasja",
-    lastName: "Pupkin",
-    phone: "015959599501",
+    lastName: 'Pupkin',
+    phone: '0176303099',
     email: 'vasja.pupkin@gmail.com',
-    address: 'Main str 1'
+    address: 'Main str 117'
   },
   {
     name: "Maria",
-    lastName: "Scmidt",
-    phone: "015959599502",
+    lastName: 'Schmidt',
+    phone: '017399910054',
     email: 'maria.schmidt@gmail.com',
-    address: 'Main str 2'
+    address: 'Hauptstr 123'
   }
 ]
+
 
 @Component({
   selector: 'app-contact-list',
   imports: [
-    ContactListElementComponent
+    ContactListElementComponent,
+    NgbAlert
   ],
   templateUrl: './contact-list.component.html',
-  standalone: true,
-  styles: ``
+  styles: ``,
+  standalone: true
 })
 export class ContactListComponent {
-   allContacts : Contact[] = CONTACTS;
+
+  allContacts = CONTACTS;
 
 }
