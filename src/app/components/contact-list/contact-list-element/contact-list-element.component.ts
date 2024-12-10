a4359f9042881e6e44479c1070da2ffc95117ad9
Maryna Matsveyenka <marina.matweenko1994@gmail.com>
2024-12-10 22:29:29 +0100

    added add-contact-form

--- src/app/components/contact-list/contact-list-element/contact-list-element.component.ts
+++ src/app/components/contact-list/contact-list-element/contact-list-element.component.ts
@@ -6,7 +6,7 @@ import {Contact} from '../../../model/contact';
   imports: [],
   template: `
     <li class="list-group-item list-group-item-action">ID: {{ (elementId + 1) }}</li>
-    <li class="list-group-item list-group-item-action">Name: {{ contactToDisplay.name }}</li>
+    <li class="list-group-item list-group-item-action">Name: {{ contactToDisplay.firstName }}</li>
     <li class="list-group-item list-group-item-action">Last name: {{ contactToDisplay.lastName }}</li>
     <li class="list-group-item list-group-item-action">Address: {{ contactToDisplay.address }}</li>
     <li class="list-group-item list-group-item-action">Phone: {{ contactToDisplay.phone }}</li>
